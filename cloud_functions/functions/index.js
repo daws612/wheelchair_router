2f5c7622325dff66645600e44948954a8dbac5dc
Firdaws <firdaws.farukh@hotmail.com>
2019-12-13 14:08:30 +0300

    Add function to compute elevation between points on a path

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -15,10 +15,14 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 exports.getDirections = functions.https.onRequest((request, response) => {
 
-    //var directionsService = googleMapsClient.DirectionsService();
+    app(request, response);
+});
+
+async function app(request, response) {
     var start = '40.763221, 29.925132'; //(40.821088, 29.926643);//(40.823665,29.925366);
-    //var end = new google.maps.LatLng(38.334818, -181.884886);
+
     var end = '40.765618, 29.925497';//(40.820627, 29.923613); //(40.820791, 29.920961); //(40.820783,29.921082);
+
     var req = {
         origin: start,
         destination: end,
@@ -28,56 +32,60 @@ exports.getDirections = functions.https.onRequest((request, response) => {
 
     googleMapsClient.directions(req, function (request, resp) {
         if (resp.status === 200) {
-            calculateSlope(resp);
-            response.send("got directions");
+            var a = calculateSlope(req, resp).then((a) => {
+                response.send(JSON.stringify(a));
+                return true;
+            },err=>
+            {
+              throw err;
+            }).catch((reason) => {
+                response.send("problem in got directions " + reason);
+            });
+            //resolve(a);
 
         } else {
             console.log("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
             response.send("problem in got directions");
         }
     });
-});
+    // var a = await calculateSlope(req, resp);
+    // return a;
+}
 
-function calculateSlope(response, map) {
+async function calculateSlope(req, response) {
+    return new Promise(function (resolve, reject) {
 
-    for (var i = 0; i < response.json.routes.length; i++) {
-        var path = [];
-        var points = response.json.routes[i].overview_polyline.points;
-        path = decode(points);
-        for (var p = 0; p < path.length - 1; p++) {
-            //get slope between these two points.
-            displayLocationElevation(path[p], path[p + 1], p, i, function (slope, index, pathIndex) {
+        var totalPoints = 0;
+        for (var i = 0; i < response.json.routes.length; i++) {
+            var polypoints = response.json.routes[i].overview_polyline.points;
+            path = decode(polypoints);
+            totalPoints = totalPoints + path.length - 1;
+        }
 
-
-                //create path with the 2 points used to get elevation only
-                var newPath = [];
-                var pathOnPoint = response.routes[pathIndex].overview_path;
-                for (var r = 0; r <= pathOnPoint.length; r++) {
-                    if (r == index || r == index + 1) {
-                        newPath.push(pathOnPoint[r]);
-                        bounds.extend(pathOnPoint[r]);
+        var receivedPath = 0;
+        var slopes = [];
+        for (var j = 0; j < response.json.routes.length; j++) {
+            var path = [];
+            var points = response.json.routes[j].overview_polyline.points;
+            path = decode(points);
+            for (var p = 0; p < path.length - 1; p++) {
+                //get slope between these two points.
+                displayLocationElevation(path[p], path[p + 1], p, j, function (err, slope, index, pathIndex) {
+                    if(err){
+                        return reject(err);
                     }
-                }
-                var polylineOptions = {
-                    path: newPath,
-                    strokeColor: colorToUse,
-                    strokeOpacity: 1,
-                    strokeWeight: 4,
-                    width: 15,
-                    geodesic: true,
-                    icons: [{
-                        icon: lineSymbols[pathIndex],
-                        offset: '0',
-                        repeat: '20px'
-                    }],
-                };
+                    receivedPath++;
+                    var loc1 = path[index];
+                    var loc2 = path[index + 1];
+                    slopes.push({ loc1, loc2, slope, pathIndex })
+                    if (receivedPath >= totalPoints) {
+                        resolve(slopes);
+                    }
+                });
 
-                var stepPolyline = new google.maps.Polyline(polylineOptions);
-                polylines.push(stepPolyline);
-            });
-
-        }//end path loop
-    }//end routes loop
+            }//end path loop
+        }//end routes loop
+    });
 
 }
 
@@ -88,9 +96,20 @@ function displayLocationElevation(location1, location2, index, pathIndex, callba
         path: [location1, location2/*{ lat: location1.latitude, lng: location1.longitude }, 
         { lat: location2.latitude, lng: location2.longitude }*/]
     },
-        function (response) {
-            if (response.status === 200) {
-                return response.json.results
+        function (err, elevationResponse) {
+            if (elevationResponse.status === 200) {
+                if(err) {
+                    callback("error reported :: " + err);
+                    return;
+                }
+
+                var run = getDistance(location1, location2);
+                var rise = elevationResponse.json.results[1].elevation - elevationResponse.json.results[0].elevation; // if negative, down slope
+                var slope = (rise / run) * 100.0;
+                console.log(slope);
+                callback(err, slope, index, pathIndex);
+                return true;
+                //req.send(JSON.stringify(elevationResponse));
             }
 
         });
@@ -142,6 +161,21 @@ function displayLocationElevation(location1, location2, index, pathIndex, callba
 //     resolution: 9.543951988220215 
 //   }]
 
+var rad = function (x) {
+    return x * Math.PI / 180;
+};
+
+var getDistance = function (p1, p2) {
+    var R = 6378137; // Earth’s mean radius in meter
+    var dLat = rad(p2.latitude - p1.latitude);
+    var dLong = rad(p2.longitude - p1.longitude);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(rad(p1.latitude)) * Math.cos(rad(p2.latitude)) *
+        Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d; // returns the distance in meter
+};
 
 // source: http://doublespringlabs.blogspot.com.br/2012/11/decoding-polylines-from-google-maps.html
 function decode(encoded) {
@@ -161,7 +195,7 @@ function decode(encoded) {
         } while (b >= 0x20);
 
 
-        var dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+        var dlat = ((result & 1) !== 0 ? ~(result >> 1) : (result >> 1));
         lat += dlat;
         shift = 0;
         result = 0;
@@ -170,7 +204,7 @@ function decode(encoded) {
             result |= (b & 0x1f) << shift;
             shift += 5;
         } while (b >= 0x20);
-        var dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+        var dlng = ((result & 1) !== 0 ? ~(result >> 1) : (result >> 1));
         lng += dlng;
 
         points.push({ latitude: (lat / 1E5), longitude: (lng / 1E5) })
